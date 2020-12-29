2d25f29f09fb320411fa5658290f82c7072466be
Ed <svqk@pm.me>
2020-12-29 10:48:55 -0600

    General

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import os
 import datetime as dt
 import time
 from array import *
+# import winsound
+
+# frequency = 2500  # Set Frequency To 2500 Hertz
+# duration = 1000  # Set Duration To 1000 ms == 1 second
+
+
 #import sleep
 
 def ping_ip(current_ip_address):
@@ -29,11 +35,15 @@ with open(startup_csv_path, 'a') as fd:
     fd.write("dt_UDM_8888_1111")
     fd.write("\n")
 
-n=1
+winsound.Beep(frequency, duration)
+
+all = 0 # 1 - Prints all result, else prints only on inet connection issues
+n = 1
 j = 1
 s_UDM = 0
 s_8 = 0
 s_1 = 0
+
 while n==1:
     i = 1
     if __name__ == '__main__':
@@ -50,10 +60,18 @@ while n==1:
     s_UDM = s_UDM + result[1]
     s_8 = s_8 + result[2]
     s_1 = s_1 + result[3]
-    output_string = (time_ping + " " + str(result[1])+ "_" + str(round(100 * s_UDM / j, 0)) + "_" +
-                     str(result[2]) + "_" + str(round(100*s_8/j, 0)) + "_" +
-                     str(result[3]) + "_" + str(round(100*s_1/j, 0)))
-    print(output_string)
+    output_string = (time_ping + " " + str(j)+ " " +
+                     str(result[1]) + "_" + str(s_UDM) + "_" + str(round(100 * s_UDM / j, 1)) + " " +
+                     str(result[2]) + "_" + str(s_8) + "_" + str(round(100*s_8/j, 1)) + " " +
+                     str(result[3]) + "_" + str(s_1) + "_" + str(round(100*s_1/j, 1)))
+    if all ==1:
+        print(output_string)
+    else:
+        print(time_ping + " " + str(j))
+        if result[2]==0 or result[3]==0:
+            print(output_string)
+            #winsound.Beep(frequency, duration)
+
     with open(startup_csv_path, 'a') as fd:
         fd.write(output_string)
         fd.write("\n")
